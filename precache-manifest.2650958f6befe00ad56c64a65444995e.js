ab9f2a4a2b758511ba06020f3988905ba6edb6e8
Roman <romabruzak@gmail.com>
2020-08-17 00:13:14 +0300

    Updates

---
+++ precache-manifest.2650958f6befe00ad56c64a65444995e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4331b73c94712dede39b88cbb1b8ff79",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "1405ad3a650109a73e44",
+    "url": "/notes/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e1202febcac26158f58f",
+    "url": "/notes/static/css/main.404e7abb.chunk.css"
+  },
+  {
+    "revision": "1405ad3a650109a73e44",
+    "url": "/notes/static/js/2.7a03d822.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/notes/static/js/2.7a03d822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1202febcac26158f58f",
+    "url": "/notes/static/js/main.11c6d06b.chunk.js"
+  },
+  {
+    "revision": "964564dd758dbba2b40f",
+    "url": "/notes/static/js/runtime-main.436451cb.js"
+  }
+]);
