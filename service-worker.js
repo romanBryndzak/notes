30fac66f9d571e4a5f8d9c815e25f256be809007
Roman <romabruzak@gmail.com>
2020-08-17 14:03:18 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notes/precache-manifest.99451b359afd6a127f41f34a64ed89b8.js"
+  "/notes/precache-manifest.863cc11a5db790e3486b37e808af7fd7.js"
 );
 
 self.addEventListener('message', (event) => {
